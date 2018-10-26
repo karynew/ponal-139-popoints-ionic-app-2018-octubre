fe59b8185e3b13aeb8498015d743dad3381b5782
Diego Aldana <dealdanav@gmail.com>
2018-10-25 21:17:16 -0500

    create services to owner and dog. Page to walk hour form

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { OwnerServiceProvider } from '../../providers/owner-service/owner-service';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the SignUpPage page.
@@ -15,15 +17,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignUpPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  dataPeople = {
+    "firstName": "",
+    "lastName": "",
+    "emailUser": "",
+    "passwordUser": "",
+    "peopleId": 2,
+    "identificationNumber": 0
+  }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public peopleService: OwnerServiceProvider, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SignUpPage');
   }
 
-  doHome() {
-    this.navCtrl.setRoot('DogSignupPage');
+  createPeople(){
+    this.peopleService.postPeople(this.dataPeople)
+      .subscribe(data => {
+        console.log(data['_body']);
+      }, error => {
+        console.log(error);
+      });
+    let alert = this.alertCtrl.create({
+      title: 'Gracias',
+      subTitle: 'Te has registrado exitosamente',
+      buttons: [
+        {
+          text: 'Registra tu mascota',
+          handler: data => {
+  					this.navCtrl.setRoot('DogSignupPage');
+  				}
+        }
+      ]
+    });
+    alert.present();
   }
 
 }
