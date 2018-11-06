eea3d66b35048e2e2c3ce07c4961580826fa7029
karina <karynew@gmail.com>
2018-11-05 20:42:46 -0500

    I have done levels Service

--- src/pages/levels/levels.ts
+++ src/pages/levels/levels.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LevelsServiceProvider } from '../../providers/levels-service/levels-service';
 
 /**
  * Generated class for the LevelsPage page.
@@ -14,12 +15,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'levels.html',
 })
 export class LevelsPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  levels: any = [];
+  constructor(public navCtrl: NavController, public navParams: NavParams, public levelsService: LevelsServiceProvider ) {
   }
-
+ 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LevelsPage');
+    this.levelsService.getLevels()
+    .subscribe((category)=>{
+      this.levels = category;
+    },
+    (error)=>{
+      console.error(error);
+    })
   }
   goBack(){
         this.navCtrl.pop();
