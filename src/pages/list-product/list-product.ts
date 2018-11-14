5c42c443e5b10febb375991b83f8479fa9fdded5
leidy <leidydi.ariza@gmail.com>
2018-11-14 18:18:39 -0500

    Update award service

--- src/pages/list-product/list-product.ts
+++ src/pages/list-product/list-product.ts
@@ -19,51 +19,18 @@ export class ListProductPage {
   constructor(public navCtrl: NavController, public navParams: NavParams, public awardService: AwardServiceProvider) {
   }
 
- // premios= {};
-  premios: any[]=[];
-  premios1= {
-            title: "",
-            description: "",
-            Num_Popoints:0,
-            Quantity_Awards:0
-  }
-  premios2= {
-    title: "",
-    description: "",
-    Num_Popoints:0,
-    Quantity_Awards:0
-}
-premios3= {
-  title: "",
-  description: "",
-  Num_Popoints:0,
-  Quantity_Awards:0
-}
+   premios:any=[];
+ 
 
 ionViewDidLoad() {
     console.log('ionViewDidLoad ListProductPage');
     this.awardService.getAward()
    
     .subscribe((award)=>{
-      //console.log(award[0].numPopoints);
-      this.premios = award[''];
+      console.log(award);
+      this.premios = award;
       
-      /*this.premios1.title = award[1].titleAward;
-      this.premios1.description= award[1].descriptionAward;
-      this.premios1.Num_Popoints= award[1].numPopoints;
-      this.premios1.Quantity_Awards= award[1].quantityAwardsAvailable;
-      
-      this.premios2.title = award[2].titleAward;
-      this.premios2.description= award[2].descriptionAward;
-      this.premios2.Num_Popoints= award[2].numPopoints;
-      this.premios2.Quantity_Awards= award[2].quantityAwardsAvailable;
-      
-      this.premios3.title = award[3].titleAward;
-      this.premios3.description= award[3].descriptionAward;
-      this.premios3.Num_Popoints= award[3].numPopoints;
-      this.premios3.Quantity_Awards= award[3].quantityAwardsAvailable;
-      */
-         },
+           },
     (error)=> {
       console.error(error);
     })
